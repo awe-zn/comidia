786bf52243a5e1798180ee0c53d0babc6ab2dfc4
Zadoque Teófilo <zadoque.dev@gmail.com>
2022-12-02 11:01:52 -0300

    feat: Adicionando arquivo de constantes para variáveis que mudam de acordo com a instalação do wordpress

--- constants.php
+++ constants.php
@@ -2,3 +2,4 @@
 
 $noticias_search_form_shortcode = '[ivory-search id="102" title="Notícias search form"]';
 $selecao_search_form_shortcode = '[ivory-search id="119" title="Seleção search form"]';
+$home_page_id = 130;
