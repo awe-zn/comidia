0aa3af313f79392db79d4fd553c21df4c73515d4
Zadoque Teófilo <zadoque.dev@gmail.com>
2022-12-05 13:28:48 -0300

    feat: Adicionando variável para novo formulário de pesquisa geral

--- constants.php
+++ constants.php
@@ -2,4 +2,5 @@
 
 $noticias_search_form_shortcode = '[ivory-search id="102" title="Notícias search form"]';
 $selecao_search_form_shortcode = '[ivory-search id="119" title="Seleção search form"]';
+$search_form_shortcode = '[ivory-search id="363" title="Search form"]';
 $home_page_id = 130;
